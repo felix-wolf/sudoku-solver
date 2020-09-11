9f4a8f6c5b626f89dfe459cc0a6af873c75e71e0
Felix Wolf <x3mdroid@gmail.com>
2020-09-11 19:01:37 +0200

    changed how fields are parsed

--- sudoku.py
+++ sudoku.py
@@ -4,22 +4,30 @@ from random import *
 root = Tk()
 buttonWidth = 3
 fields = []
+# field[COLUMN][ROW]
 field = [[0 for x in range(9)] for y in range(9)]
 
-def close_window():
-	root.destroy()
-
-def readAllNumbers():
-	for i in range(8):
-		for k in range (8):
-			field[i][k] = fields[(i * 10) + k].get()
-			print(field[i][k])
+def compute():
+	readAllNumbers()
 	calculateSolution()
 
+def readAllNumbers():
+	i = 0
+	k = 0
+	for item in fields:
+		field[i][k] = item.get()
+		i = i + 1
+		if i is 9:
+			i = 0
+			k = k + 1
+
 def calculateSolution():
-	#print(checkRow(0, 0))
-	print(checkBox(1, 0))
-	#print(randint(0, 9))
+	for i in range(9):
+		for k in range (9):
+			if field[i][k] == "":
+				print("find Number")
+			else:
+				print("false")
 
 def checkRow(column, row):
 	number = field[column][row]
@@ -37,25 +45,6 @@ def checkColumn(column, row):
 			numberOfDuplicates +=1
 	return numberOfDuplicates is 1
 
-def checkBox(column, row):
-	verticalSection = 1
-	horizontalSection = 1
-	if (4 <= row <=6):
-		verticalSection = 2
-	if (7 <= row <= 9):
-		verticalSection = 3
-
-	if (4 <= row <=6):
-		horizontalSection = 2
-	if (7 <= row <= 9):
-		horizontalSection = 3
-	number = field[column][row]
-	numberOfDuplicates = 0
-	for i in range(9):
-		if field[column][i] == number:
-			numberOfDuplicates +=1
-	return numberOfDuplicates is 1
-
 def setupFields():
 	index = 0
 	for i in range(9):
@@ -66,13 +55,15 @@ def setupFields():
 			fields.append(button)
 
 def setupQuitButton():
-	button = Button(root, width = buttonWidth * 3, text = "QUIT", command = close_window)
+	button = Button(root, width = buttonWidth * 2, text = "QUIT", command = close_window)
 	button.grid(row = 11, column = 0, columnspan = 2)
 
 def setupComputeButton():
-	button = Button(root, width = buttonWidth * 10, text = "COMPUTE", command = readAllNumbers)
+	button = Button(root, width = buttonWidth * 10, text = "COMPUTE", command = compute)
 	button.grid(row = 11, column = 2, columnspan = 7)
 
+def close_window():
+	root.destroy()
 
 def main():
 	setupFields()
