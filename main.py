54cbcffc1cc6d9dbf78324fdc4776a745a0bb884
Felix Wolf <x3mdroid@gmail.com>
2021-12-26 00:48:04 +0100

    add new files, start working with numpy arrays in main.py file

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def main():
+    print("Hello World!")
+    array = np.array([
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+        np.zeros(9),
+    ])
+    print(array)
+    print(array.size)
+
+
+if __name__ == "__main__":
+    main()
